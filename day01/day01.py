20f83bd0c3add3e9e5f0c2eb70c378eb6db45b7f
Lewis Staples <lewis.staples@gmail.com>
2022-09-15 15:46:41 -0400

    Day 01 Parts A and B complete

--- day01/day01.py
+++ day01/day01.py
@@ -2,10 +2,11 @@
 # https://adventofcode.com/2017/day/01
 
 
-num_pairs = 0
+num_pairs_a = 0
+num_pairs_b = 0
 
 # Reading input from the input file
-input_filename='input_sample0.txt'
+input_filename='input_sample4.txt'
 print(f'\nUsing input file: {input_filename}')
 with open(input_filename) as f:
     in_string = f.readline().rstrip()
@@ -16,10 +17,14 @@ else:
     print()
 
 for i1 in range(len(in_string)):
-    i2 = (i1 + 1) % len(in_string)
-    
-    if in_string[i1] == in_string[i2]:
-        num_pairs += int(in_string[i1])
+    i2a = (i1 + int(len(in_string)+1)) % len(in_string)
+    i2b = (i1 + int(len(in_string)/2)) % len(in_string)
 
-print(f'The answer to part A is {num_pairs}\n')
+    if in_string[i1] == in_string[i2a]:
+        num_pairs_a += int(in_string[i1])
+    if in_string[i1] == in_string[i2b]:
+        num_pairs_b += int(in_string[i1])
+
+print(f'The answer to part B is {num_pairs_a}')
+print(f'The answer to part B is {num_pairs_b}\n')
 
