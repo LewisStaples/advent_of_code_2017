12776acca9ff3031d17d41b9cb9eb2b0aafb5b49
Lewis Staples <lewis.staples@gmail.com>
2022-11-01 17:25:50 -0400

    Day 06 Code cleanup (improving variable names, improved documentation)

--- day06/day06.py
+++ day06/day06.py
@@ -2,17 +2,24 @@
 # https://adventofcode.com/2017/day/16
 
 
+# Perform a single redistribution cycle
 def redistribute(memory_banks):
     max_bank = max(memory_banks)
     max_bank_index = memory_banks.index(max_bank)
     memory_banks[max_bank_index] = 0
-    bank_indices_with_another_block = set()
+
+    # Identify the banks that will receive an additional block
+    bank_indices_to_receive_one_more_block = set()
     for i in range(max_bank % len(memory_banks)):
-        bank_indices_with_another_block.add((i + max_bank_index + 1) % len(memory_banks))
+        bank_indices_to_receive_one_more_block.add((i + max_bank_index + 1) % len(memory_banks))
+
     for i in range(len(memory_banks)):
+        # All banks start by getting equal shares of the distribution
         memory_banks[i] += max_bank//len(memory_banks)
-        if i in bank_indices_with_another_block:
+        # And some banks get one additional block
+        if i in bank_indices_to_receive_one_more_block:
             memory_banks[i] += 1
+            
     return memory_banks
 
 # Reading input from the input file
@@ -22,19 +29,18 @@ with open(input_filename) as f:
     in_string = f.readline().rstrip()
    
 memory_banks = [int(x) for x in in_string.split()]
-hash_all_memory_banks = {hash(tuple(memory_banks)): 0}
+all_hashes__and__redist_cycle_count = {hash(tuple(memory_banks)): 0}
 redist_cycle_count = 0
 
 while True:
     redist_cycle_count += 1
     memory_banks = redistribute(memory_banks)
     hash_current_memory_banks = hash(tuple(memory_banks))
-    if hash_current_memory_banks not in hash_all_memory_banks:
-        # hash_all_memory_banks.add(hash_current_memory_banks)
-        hash_all_memory_banks[hash_current_memory_banks] = redist_cycle_count
+    if hash_current_memory_banks not in all_hashes__and__redist_cycle_count:
+        all_hashes__and__redist_cycle_count[hash_current_memory_banks] = redist_cycle_count
     else:
         # This configuration has been seen before
         break
 
 print(f'The answer to part A is: {redist_cycle_count}')
-print(f'The answer to part B is: {redist_cycle_count - hash_all_memory_banks[hash_current_memory_banks]}\n')
+print(f'The answer to part B is: {redist_cycle_count - all_hashes__and__redist_cycle_count[hash_current_memory_banks]}\n')
