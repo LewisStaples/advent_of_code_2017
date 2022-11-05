22354ff6253fd82f56093a1c96581824309027d2
Lewis Staples <lewis.staples@gmail.com>
2022-11-05 16:46:05 -0400

    Day 09 Part B complete

--- day09/day09.py
+++ day09/day09.py
@@ -28,20 +28,23 @@ def perform_cancels(in_string):
     return in_string
 
 def process_garbage(in_string):
-    ret_val = ''
+    ret_str = ''
+    ret_count = 0
     string_i = 0
     while string_i < len(in_string):
         if in_string[string_i] == '>':
             index_lt = in_string.index('<')
+            ret_count += max((string_i - index_lt - 1), 0)
             in_string = in_string[:index_lt] + in_string[string_i + 1:]
             string_i -= (string_i - index_lt)
+            
         string_i += 1
     
-    ret_val += in_string
-    return ret_val
+    ret_str += in_string
+    return ret_str, ret_count
 
 # This assumes that perform_cancels and process_garbage have already been run
-def get_score_internal(in_string):
+def get_score(in_string):
     ret_val = 0
     string_i = 0
     brace_level = 0
@@ -54,24 +57,28 @@ def get_score_internal(in_string):
         string_i += 1
     return ret_val
 
-def get_score(in_string):
+
+def get_results(in_string):
     in_string = perform_cancels(in_string)
     conditional_data_display('After cancelling', in_string)
 
-    in_string = process_garbage(in_string)
+    in_string, garbage_count = process_garbage(in_string)
     conditional_data_display('After processing garbage', in_string)
 
-    return get_score_internal(in_string)
+    return get_score(in_string), garbage_count
 
 # Reading input from the input file
-input_filename='input_sample0.txt'
+input_filename='input_sample1.txt' # 'input_sample1.txt' is the input for part 2/B, whereas 'input_sample0.txt' is input for part 1/A
 print(f'\nUsing input file: {input_filename}\n')
 with open(input_filename) as f:
     # Pull in each line from the input file
     for in_string in f:
         in_string = in_string.rstrip()
         conditional_data_display('Input', in_string)
-        print(f'Result: {get_score(in_string)}\n')
+        the_results = get_results(in_string)
+
+        print(f'\nThe total score is {the_results[0]}')
+        print(f'The count of non-canceled characters within the garbage is {the_results[1]}\n\n')
 
 
 def test_cancel():
