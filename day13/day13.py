995bc403d27b4c3b3f1046fa636d711f0f1de745
Lewis Staples <lewis.staples@gmail.com>
2022-11-12 23:33:48 -0500

    Day 13 Was previously misusing the term depth.  Most of the incorrect uses were replaced with height ... betwen top and bottom.  Another incorrect use was replaced with range.

--- day13/day13.py
+++ day13/day13.py
@@ -2,7 +2,7 @@
 # https://adventofcode.com/2017/day/13
 
 
-def get_curr_depth(elap_time, range):
+def get_curr_height(elap_time, range):
     # known:  range:2,period:2; range:3,period:4, range:4,period:6
     period = (range - 1) * 2
     # Taking elap_time % period simplifies things
@@ -17,7 +17,7 @@ layer__depth_to_range = dict()
 
 # Reading input from the input file
 input_filename='input_sample0.txt'
-print(f'\nUsing input file: {input_filename}\n')
+print(f'\nUsing input file: {input_filename}')
 with open(input_filename) as f:
     # Pull in each line from the input file
     for in_string in f:
@@ -25,38 +25,49 @@ with open(input_filename) as f:
         print(in_string)
         k, v = in_string.split(': ')
         layer__depth_to_range[int(k)] = int(v)
+    print()
 
+for elap_time in range(max(layer__depth_to_range.keys()) + 1):
+    print(elap_time, end=': ')
+    print()
 
-def test_get_curr_depth():
-    # Consider a layer with a depth of two
-    assert get_curr_depth(0, 2) == 0
-    assert get_curr_depth(1, 2) == 1
-    assert get_curr_depth(2, 2) == 0
-    assert get_curr_depth(3, 2) == 1
-    assert get_curr_depth(4, 2) == 0
+def test_get_curr_height():
+    # Consider a layer with a range of two
+    assert get_curr_height(0, 2) == 0
+    assert get_curr_height(1, 2) == 1
+    assert get_curr_height(2, 2) == 0
+    assert get_curr_height(3, 2) == 1
+    assert get_curr_height(4, 2) == 0
 
-    # Consider a layer with a depth of four
-    assert get_curr_depth(0, 4) == 0
-    assert get_curr_depth(1, 4) == 1
-    assert get_curr_depth(2, 4) == 2
-    assert get_curr_depth(3, 4) == 3
-    assert get_curr_depth(4, 4) == 2
-    assert get_curr_depth(5, 4) == 1
-    assert get_curr_depth(6, 4) == 0
+    # Consider a layer with a range of three
+    assert get_curr_height(0, 3) == 0
+    assert get_curr_height(1, 3) == 1
+    assert get_curr_height(2, 3) == 2
+    assert get_curr_height(3, 3) == 1
+    assert get_curr_height(4, 3) == 0
 
-    # Consider a layer with a depth of eight
-    assert get_curr_depth(0, 8) == 0
-    assert get_curr_depth(1, 8) == 1
-    assert get_curr_depth(2, 8) == 2
-    assert get_curr_depth(3, 8) == 3
-    assert get_curr_depth(4, 8) == 4
-    assert get_curr_depth(5, 8) == 5
-    assert get_curr_depth(6, 8) == 6
-    assert get_curr_depth(7, 8) == 7
-    assert get_curr_depth(8, 8) == 6
-    assert get_curr_depth(9, 8) == 5
-    assert get_curr_depth(10, 8) == 4
-    assert get_curr_depth(11, 8) == 3
-    assert get_curr_depth(12, 8) == 2
-    assert get_curr_depth(13, 8) == 1
-    assert get_curr_depth(14, 8) == 0
+    # Consider a layer with a range of four
+    assert get_curr_height(0, 4) == 0
+    assert get_curr_height(1, 4) == 1
+    assert get_curr_height(2, 4) == 2
+    assert get_curr_height(3, 4) == 3
+    assert get_curr_height(4, 4) == 2
+    assert get_curr_height(5, 4) == 1
+    assert get_curr_height(6, 4) == 0
+
+    # Consider a layer with a range of eight
+    assert get_curr_height(0, 8) == 0
+    assert get_curr_height(1, 8) == 1
+    assert get_curr_height(2, 8) == 2
+    assert get_curr_height(3, 8) == 3
+    assert get_curr_height(4, 8) == 4
+    assert get_curr_height(5, 8) == 5
+    assert get_curr_height(6, 8) == 6
+    assert get_curr_height(7, 8) == 7
+    assert get_curr_height(8, 8) == 6
+    assert get_curr_height(9, 8) == 5
+    assert get_curr_height(10, 8) == 4
+    assert get_curr_height(11, 8) == 3
+    assert get_curr_height(12, 8) == 2
+    assert get_curr_height(13, 8) == 1
+    assert get_curr_height(14, 8) == 0
