d0fba2914cf55139735ddfa59d7fe26a32aaa826
Lewis Staples <lewis.staples@gmail.com>
2023-08-18 20:56:58 -0400

    Day 20:  Revised simpler approach.  Now the farthest is which particle has largest abs(a) ... if tie then abs(v) ... if tie then abs(p)

--- day20/day20.py
+++ day20/day20.py
@@ -4,32 +4,14 @@
 # https://adventofcode.com/2017/day/20
 
 
-class Axis:
-    def __init__(self, input):
-        (init_p, init_v, init_a) = input
-        self.INIT_P = init_p
-        self.INIT_V = init_v
-        self.INIT_A = init_a
-
-
 class Particle:
     def __init__(self, in_string):
         p_string, remainder_string = in_string.split('v')
         v_string, a_string = remainder_string.split('a')
-        # self.axes = self.get_axes_from_input(p_string, v_string, a_string)
-        self.axes = {f'axis # {k}':v for k,v in enumerate(self.get_axes_from_input(p_string, v_string, a_string))}
 
-        dummy = 123
-
-
-    def get_axes_from_input(self, p_string, v_string, a_string):
-        p_values = self.get_values_from_string(p_string)
-        v_values = self.get_values_from_string(v_string)
-        a_values = self.get_values_from_string(a_string)
-        ret_val = list()
-        for item in zip(p_values, v_values, a_values):
-            ret_val.append(Axis(item))
-        return ret_val
+        self.p_values = self.get_values_from_string(p_string)
+        self.v_values = self.get_values_from_string(v_string)
+        self.a_values = self.get_values_from_string(a_string)
 
     def get_values_from_string(self, in_string):
         in_string = in_string[in_string.index('<')+1:]
@@ -55,28 +37,55 @@ def get_input(input_filename):
     return the_particles
 
 
-def get_axis_values_0_1_2(the_particles):
-    ret_val = dict()
+def get_sum_abs(in_list):
+    ret_val = 0
+    for ele in in_list:
+        ret_val += abs(ele)
+    return ret_val
+
+
+def some_fxn(the_particles):
+    largest_seen_values = {'p':0, 'v':0, 'a':0}
+    for particle_number_label, the_particle in the_particles.items():
+        particle_number_int = int(particle_number_label[11:])
+        dummy = 123
+        for variable in largest_seen_values:
+            values_member_str = variable + '_values'
+            new_value = get_sum_abs(getattr(the_particle, values_member_str))
+            if largest_seen_values[variable] is None:
+                pass
+            elif new_value == largest_seen_values[variable]:
+                largest_seen_values[variable] = None
+            elif new_value > largest_seen_values[variable]:
+                largest_seen_values[variable] = new_value
+
+
+            dummy = 123
+
     dummy = 123
+    for something1, something2 in largest_seen_values.items():
+        if something2 is None:
+            continue
 
+        # # Loop through axes
+        # for i in range(len(the_particle.a_values)):
+        #     dummy = 123
+            
+# bar = getattr(foo, 'bar')
+# result = bar()
 
-'''
-My math predicts that the distance along any axis should be polynomial two.
-Thus, there are coefficients  d,e,f where   d*t^2 + e*t + f = distance along axis.
-Using a,b,c and doubling, I get     a*t^2 + b*t + c = 2*(distance along axis)
-And if I know the distance at times 0,1,2:
-a = distance(0) - distance(2)
-b = 2 * distance(1) - distance(0) - distance(2)
-c = 2 * distance(0)
-'''
-def get_coefficients(the_particles, axis_values_0_1_2):
-    pass
-
+        # Develop lists of pointers to the greatest abs value of a_values axes by going from one particle to the next one
+        # If a tie is needed, go to v_values and then p_values.
+    
+    # return whichever particle is closest to <0,0,0> in the long term
 
 def solve_problem(input_filename):
     the_particles = get_input(input_filename)
-    axis_values_0_1_2 = get_axis_values_0_1_2(the_particles)
-    the_coefficients = get_coefficients(the_particles, axis_values_0_1_2)
+    some_fxn(the_particles)
+    dummy = 123
+
+    # axis_values_0_1_2 = get_axis_values_0_1_2(the_particles)
+    # the_coefficients = get_coefficients(the_particles, axis_values_0_1_2)
 
 
 
